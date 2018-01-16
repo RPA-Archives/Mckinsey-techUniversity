0caa476dba64bec95c7ccfba5c752d681689a6f7
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-01-16 12:10:00 +0530

    function exercise two complete

---
+++ function/exercise2.js
@@ -0,0 +1,22 @@
+let counter = 0;
+function repeat (inputFunction, noOfRepeat){
+	if (noOfRepeat === 0) { 
+		return counter;
+	} else if (noOfRepeat > 0) {
+		inputFunction();
+		return repeat (inputFunction, --noOfRepeat);
+	} else {
+		return null;
+	}
+}
+
+function incCount(){
+ counter++;
+}
+
+console.log("Should pass the basic test case: ", repeat(incCount, 4) === 4);
+counter=0;
+console.log("Should pass the base test case: ", repeat(incCount, 0) === 0);
+counter=0;
+console.log("Should pass the fail test case: ", repeat(incCount, -4) === null);
+// module.exports = repeat;
