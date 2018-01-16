34d257dd73eb75e89e3ae33d2de643e3055cdfc4
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-01-16 10:02:06 +0530

    hello world test case pass

---
+++ function/index.js
@@ -0,0 +1,7 @@
+function upperCaser(inputString){
+	return inputString.toUpperCase();
+}
+
+
+//console.log("Should print the 'Hello' string in upper case:", upperCaser("hello") === "HELLO");
+module.exports = upperCaser
